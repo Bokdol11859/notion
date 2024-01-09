d87f43b6387036ed0bfc942757c92ea31e46f791
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-01-09 23:22:44 +0900

    feat: implement github oauth with clerk

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,25 +1,46 @@
 "use client";
 
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 
 const Heading = React.memo(() => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
-        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
-        <span className="underline">Notion</span>
+        Your Ideas, Documents, & Plans. Unified.
+        <br />
+        Welcome to <span className="underline">Notion</span>
       </h1>
       <h3 className="text-base font-medium sm:text-xl md:text-2xl">
         Notion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Notion
-        <ArrowRight className="ml-2 h-4 w-4" />
-      </Button>
+      {isLoading ? (
+        <div className="flex w-full items-center justify-center">
+          <Spinner />
+        </div>
+      ) : isAuthenticated && !isLoading ? (
+        <Button asChild>
+          <Link href={"/documents"}>
+            Enter Notion
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
+      ) : (
+        <SignInButton mode="modal">
+          <Button size={"sm"}>
+            Enter Notion <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 });
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,25 +1,54 @@
 "use client";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 import React from "react";
 
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Spinner } from "@/components/ui/spinner";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 
 import Logo from "./logo";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop(20);
 
   return (
     <div
       className={cn(
-        "fixed top-0 z-50 flex w-full items-center bg-background p-6 dark:bg-[#1F1F1F]",
+        "fixed top-0 z-50 flex w-full items-center bg-background p-6  dark:bg-[#1F1F1F] ",
         scrolled && "border-b shadow-sm",
       )}
     >
-      <div className="flex w-full items-center justify-between gap-x-2 text-black text-muted-foreground md:ml-auto">
+      <div className="flex w-full items-center justify-between gap-x-2  text-black text-muted-foreground md:ml-auto dark:text-white">
         <Logo />
-        <ModeToggle />
+        <div className="mr-2 flex items-center">
+          {isLoading && <Spinner className="mr-2" />}
+          {!isAuthenticated && !isLoading && (
+            <div className="mr-2 flex gap-2">
+              <SignInButton mode="modal">
+                <Button variant={"ghost"} size={"sm"}>
+                  Log In
+                </Button>
+              </SignInButton>
+              <SignInButton mode="modal">
+                <Button size={"sm"}>Learn More</Button>
+              </SignInButton>
+            </div>
+          )}
+          {isAuthenticated && !isLoading && (
+            <div className="mr-4 flex items-center gap-2">
+              <Button variant={"ghost"} size={"sm"} asChild>
+                <Link href={"/documents"}>Documents</Link>
+              </Button>
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          )}
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
